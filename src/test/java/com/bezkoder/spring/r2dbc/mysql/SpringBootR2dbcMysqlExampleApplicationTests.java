d6feb22e1e733878b5b3f78dcb35640744f1e087
tienbku <tien.ngtruong@gmail.com>
2023-02-13 10:36:53 +0700

    first commit

---
+++ src/test/java/com/bezkoder/spring/r2dbc/mysql/SpringBootR2dbcMysqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bezkoder.spring.r2dbc.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootR2dbcMysqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
